8494150040c236084fc33fb18df83c4f8f770e20
Christian Effiong <norexus007@gmail.com>
2017-11-27 04:20:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PreVue
+--------------------------------------------
+PreVue is an music search web app that allows users preview music contents from iTunes for a duration of 30 seconds.
+
+This work is still in progress. Other functionalities will be added soon.
---
+++ js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(() => {
+    console.log('working');
+   $('#searchForm').on('submit', (e) => {
+      let searchText = $('#searchText').val();
+      getMusic(searchText);
+      e.preventDefault();
+   });
+});
+
+function getMusic(searchText) {
+    axios.get('https://itunes.apple.com/search?term='+searchText)
+        .then((response) => {
+            console.log(response);
+            let music = response.data.results;
+            let output = '';
+            $.each(music, (index, album) => {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${album.artworkUrl100}">
+                        <h5>${album.trackName}</h5>
+                        <audio controls><source src="${album.previewUrl}"></audio>
+                        <br>
+                        <a onclick="musicSelected('${album.artistId}')" class="btn btn-primary" href="#">Artist Details</a>
+                    </div>
+                </div>
+                `;
+            });
+
+            $('#music').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function musicSelected(id) {
+    sessionStorage.setItem('artistId', id);
+    window.location = 'music.html';
+    return false;
+}
+
+function getTrack() {
+    let artistId = sessionStorage.getItem('artistId');
+
+    axios.get('https://itunes.apple.com/lookup?id='+artistId)
+        .then((response) => {
+            console.log(response);
+           /* let music = response.data.results;
+            let output = '';
+            $.each(music, (index, album) => {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${album.artworkUrl100}">
+                        <h5>${album.trackName}</h5>
+                        <audio controls><source src="${album.previewUrl}"></audio>
+                        <br>
+                        <a onclick="musicSelected('${album.trackId}')" class="btn btn-primary" href="#">Album Details</a>
+                    </div>
+                </div>
+                `;
+            });
+
+            $('#music').html(output); */
+           let artist = response.data;
+           console.log($(artist.artistLinkUrl));
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    // }
+}
